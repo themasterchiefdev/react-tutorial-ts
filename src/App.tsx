d4875c3a39195b834e456ba07eda009fa457f981
YvrKarthik <yvrkarthik@gmail.com>
2021-02-28 16:37:21 +1300

    Finished module-1

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,41 @@ import './App.css';
 
 const App = () => (
     <div className="App">
-        <Button />
+        <AppWrapper/>
+
     </div>
 );
 
-function Button(){
-    const [counter,setCounter]=useState(0);
-    return <button onClick={()=>setCounter((counter+1)*2)} >{counter}</button>
+function Button(buttonProps: IButtonProps) {
+    const handleClick = () => buttonProps.onClickFunction(buttonProps.incrementValue);
+    return (<button onClick={handleClick}>+{buttonProps.incrementValue}</button>);
 }
 
+interface IDisplayProps {
+    message: number;
+}
+
+interface IButtonProps {
+    onClickFunction: any;
+    incrementValue: number;
+}
+
+function Display(displayProps: IDisplayProps) {
+    return (<div>{displayProps.message}</div>);
+}
+
+function AppWrapper() {
+    const [counter, setCounter] = useState(0);
+    let incrementCounter = (increment: number) => setCounter((counter + increment));
+    return (
+        <>
+            <Button onClickFunction={incrementCounter} incrementValue={1}/>
+            <Button onClickFunction={incrementCounter} incrementValue={5}/>
+            <Button onClickFunction={incrementCounter} incrementValue={10}/>
+            <Button onClickFunction={incrementCounter} incrementValue={50}/>
+            <Button onClickFunction={incrementCounter} incrementValue={100}/>
+            <Display message={counter}/>
+        </>
+    );
+};
 export default App;
